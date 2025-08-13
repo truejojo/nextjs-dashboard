4d961f05fb9637cb0f9eab006fe1df19df513675
truejojo <johannesveh@yahoo.de>
2025-08-13 09:10:24 +0200

    Creating Layouts and Pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Customer Page</div>;
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Invoice Page</div>;
+};
+
+export default Page;
