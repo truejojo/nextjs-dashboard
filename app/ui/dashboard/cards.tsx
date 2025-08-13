8c7c0d9a249acd9c62df12d45e015a2e1da18071
truejojo <johannesveh@yahoo.de>
2025-08-13 15:30:50 +0200

    chapter 7: fetching data

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -42,10 +42,10 @@ export function Card({
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
         className={`${lusitana.className}
