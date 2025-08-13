999ca705d6725aadcecabcfb7b96f37e94774dd2
truejojo <johannesveh@yahoo.de>
2025-08-13 08:29:12 +0200

    Chapter 3: Optimieren von Schriftarten und Bildern

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google';
+import { Merriweather } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: '300',
+});
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
