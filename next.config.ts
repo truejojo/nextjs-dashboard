a3fe325aba0c79d562f84309a6cc0cd436a81989
truejojo <johannesveh@yahoo.de>
2025-08-14 08:34:51 +0200

    Partial Prerendering - at this time not activated: experimentel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // PPR PartialPrerendering
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
